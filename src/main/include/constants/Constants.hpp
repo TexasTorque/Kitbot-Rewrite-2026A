4ad381273b3a141eb6f1b7e6e060184e0b6e9c2d
Rohan Modi <rohanmodi1014@gmail.com>
2026-05-14 21:49:54 -0500

    build: add initial project dependencies and config

---
+++ src/main/include/constants/Constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace RobotConstants {
+inline constexpr int kDriverControllerPort = 0;
+}
+
+namespace launcherConstants {
+inline constexpr int kRollerPort = 4;
+}
---
+++ src/main/include/subsystems/launcherSubsystem.hpp
@@ -0,0 +1,7 @@
+# pragma once
+# include <frc2/command/SubsystemBase.h>
+#include "constants/Constants.hpp"
+#include "frc2/command/CommandPtr.h"
+#include "rev/SparkLowLevel.h"
+#include "rev/SparkMax.h"
+#include "units/voltage.h"
